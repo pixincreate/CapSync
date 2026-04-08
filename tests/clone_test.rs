5dbe4001cd7b6dc958919218b29e307689e27e94
PiX <69745008+pixincreate@users.noreply.github.com>
2026-04-09 01:00:27 +0530

    test: add clone module tests
    Test parse_repo_url with:
    - Full https/http URLs
    - Git SSH URLs
    - Non-GitHub providers (Codeberg, GitLab, Bitbucket)
    - Whitespace/trailing slash trimming
    - Invalid URL handling

---
+++ tests/clone_test.rs
@@ -0,0 +1,58 @@
+use capsync::clone::parse_repo_url;
+
+#[test]
+fn test_parse_full_https_url() {
+    let result = parse_repo_url("https://github.com/user/repo").unwrap();
+    assert_eq!(result, "https://github.com/user/repo.git");
+
+    let result2 = parse_repo_url("https://github.com/user/repo.git").unwrap();
+    assert_eq!(result2, "https://github.com/user/repo.git");
+}
+
+#[test]
+fn test_parse_full_http_url() {
+    let result = parse_repo_url("http://github.com/user/repo").unwrap();
+    assert_eq!(result, "http://github.com/user/repo.git");
+}
+
+#[test]
+fn test_parse_git_ssh_url() {
+    let result = parse_repo_url("git@github.com:user/repo").unwrap();
+    assert_eq!(result, "git@github.com:user/repo.git");
+
+    let result2 = parse_repo_url("git@github.com:user/repo.git").unwrap();
+    assert_eq!(result2, "git@github.com:user/repo.git");
+}
+
+#[test]
+fn test_parse_non_github_url() {
+    let result = parse_repo_url("https://codeberg.org/user/repo").unwrap();
+    assert_eq!(result, "https://codeberg.org/user/repo.git");
+
+    let result2 = parse_repo_url("https://gitlab.com/user/repo.git").unwrap();
+    assert_eq!(result2, "https://gitlab.com/user/repo.git");
+
+    let result3 = parse_repo_url("git@bitbucket.org:user/repo.git").unwrap();
+    assert_eq!(result3, "git@bitbucket.org:user/repo.git");
+}
+
+#[test]
+fn test_parse_url_trims_whitespace() {
+    let result = parse_repo_url("  https://github.com/user/repo  ").unwrap();
+    assert_eq!(result, "https://github.com/user/repo.git");
+}
+
+#[test]
+fn test_parse_url_trims_trailing_slash() {
+    let result = parse_repo_url("https://github.com/user/repo/").unwrap();
+    assert_eq!(result, "https://github.com/user/repo.git");
+}
+
+#[test]
+fn test_parse_invalid_url() {
+    let result = parse_repo_url("not-a-url");
+    assert!(result.is_err());
+
+    let result2 = parse_repo_url("");
+    assert!(result2.is_err());
+}
