4712475f502cdb476f67332c00a9f8334fe329e2
PiX <69745008+pixincreate@users.noreply.github.com>
2026-04-12 17:25:40 +0530

    refactor: simplify binary entrypoint

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,5 @@
-mod cli;
-mod clone;
-mod config;
-mod detect;
-mod install;
-mod sync;
-mod tools;
-
 use anyhow::Result;
-use cli::run;
 
 fn main() -> Result<()> {
-    run()
+    capsync::cli::run()
 }
